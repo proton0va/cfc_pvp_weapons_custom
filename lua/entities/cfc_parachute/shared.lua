d976dc8d62f99b0ffa00b45419a2110e7fad5722
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2024-02-04 16:32:04 -0700

    Move parachutes to a separate repo (#62)

--- lua/entities/cfc_parachute/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
-
-ENT.PrintName       = "Parachute"
-ENT.Author          = "TwoLemons"
-ENT.Contact         = "https://discord.gg/cfc-servers"
-ENT.Purpose         = "Gliding around in safety and style"
-ENT.Instructions    = "spawnmenu > options > CFC > Parachutes for more info"
-ENT.RenderGroup     = RENDERGROUP_BOTH
